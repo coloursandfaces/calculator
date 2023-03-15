b480034082a6c9124dc621ce7c821e1f74cabf62
coloursandfaces <victorouyang98@gmail.com>
2023-03-15 13:35:44 -0700

    Checked the code for any gotcha's

--- script.js
+++ script.js
@@ -32,6 +32,13 @@ const operate = function(oper, num1, num2) {
     return oper(num1, num2);
   }
 
+function clearVariables() {
+  num1 = 0;
+  num2 = 0;
+  displayValue = 0;
+  screen.textContent = "";
+  holdOperate = null;
+}
 
 
 const buttons = document.querySelectorAll('button');
@@ -42,63 +49,74 @@ let displayValue = 0
 
 let num1 = 0;
 let num2 = 0;
+let currentResult = 0;
 let holdOperate = null;
 buttons.forEach((button) => {
-    
-
     button.addEventListener('click', function() {
         const screenDiv = document.createElement('div');
         screenDiv.textContent = button.textContent;
         screen.appendChild(screenDiv);
         displayValue = parseInt(screen.textContent);
 
-        if (button.textContent === "+") {
-            num1 = displayValue;
-            displayValue = 0;
-            screen.textContent = 0
-            holdOperate = add;
-        }
-
-        if (button.textContent === "-") {
-            num1 = displayValue;
-            displayValue = 0;
-            screen.textContent = 0
-            holdOperate = subtract;
-        }
-
-        if (button.textContent === "x") {
-            num1 = displayValue;
-            displayValue = 0;
-            screen.textContent = 0
-            holdOperate = multiply;
-        }
-
-        if (button.textContent === "%") {
-            num1 = displayValue;
-            displayValue = 0;
-            screen.textContent = 0
-            holdOperate = divide;
-        }
-
-        if (button.textContent === "c") {
-            num1 = 0
-            num2 = 0
-            displayValue = 0;
-            screen.textContent = 0
-        }
-
-        if (button.textContent === "=" && holdOperate != null) {
+        if (holdOperate != null && num1 != 0) {
+          if (holdOperate === divide && displayValue === 0) {
+            alert("You can't divide by zero!");
+            clearVariables();
+          } else {
             num2 = displayValue;
             displayValue = operate(holdOperate, num1, num2);
             screen.textContent = displayValue;
+            num1 = displayValue;
+            num2 = 0;
+            holdOperate = null;
+          }
+        } else {
+          currentResult = displayValue;
+          screen.textContent = currentResult;
         }
+
+        if (button.textContent === "+") {
+          num1 = displayValue;
+          displayValue = 0;
+          screen.textContent = ""
+          holdOperate = add;
+        }
+
+        if (button.textContent === "-") {
+          num1 = displayValue;
+          displayValue = 0;
+          screen.textContent = ""
+          holdOperate = subtract;
+        }
+
+        if (button.textContent === "x") {
+          num1 = displayValue;
+          displayValue = 0;
+          screen.textContent = ""
+          holdOperate = multiply;
+        }
+
+        if (button.textContent === "%") {
+          num1 = displayValue;
+          displayValue = 0;
+          screen.textContent = ""
+          holdOperate = divide;
+        }
+
+        if (button.textContent === "c") {
+          num1 = 0
+          num2 = 0
+          displayValue = 0;
+          screen.textContent = ""
+        }
+
+        if (button.textContent === "=" && holdOperate != null) {
+          num2 = displayValue;
+          displayValue = operate(holdOperate, num1, num2);
+          screen.textContent = displayValue;
+        }
+
     })
 })
-
-const calculate = function(...number) {
-    let num1 = 0
-    let num2 = 0
-
-}
 
 
